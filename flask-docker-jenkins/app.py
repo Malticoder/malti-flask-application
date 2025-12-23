819b267e49f94d2ca284abc7d2f1a0e8b3a9ddba
maltidemo <150908958+Malticoder@users.noreply.github.com>
2025-12-23 19:28:50 +0530

    Fix formatting issues in app.py

--- flask-docker-jenkins/app.py
+++ flask-docker-jenkins/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask 
 
 app = Flask(__name__)
 
@@ -8,6 +8,7 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
 
 
 
